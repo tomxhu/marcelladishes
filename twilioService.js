cc676cf57e0df753a64bc68257137e129aa3713f
Tommy Hu <hustheman739@gmail.com>
2014-11-12 23:49:24 -0500

    forgot return

--- twilioService.js
+++ twilioService.js
@@ -8,21 +8,23 @@ var authToken = 'abd184322d8698d41ec413d11b72e6b6';
 // require the Twilio module and create a REST client
 var client = twilio(accountSid, authToken);
 
-module.exports = function(){
-    sendText: function(number, body) {
-        var now = new Date();
-        var hour =  now.getHours();
-        if (hour > 8) {
-            client.messages.create({
-                to: number,
-                from: "+15082326612",
-                body: body,
-            }, function(err, message) {
-                if (err) {
-                    console.log(err);
-                }
-                console.log('This is the messageID', message);
-            });
+module.exports = function() {
+    return {
+        sendText: function(number, body) {
+            var now = new Date();
+            var hour =  now.getHours();
+            if (hour > 8) {
+                client.messages.create({
+                    to: number,
+                    from: "+15082326612",
+                    body: body,
+                }, function(err, message) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log('This is the messageID', message);
+                });
+            }
         }
     };
 }
