4db34e806c273938c7dbcbc00076a5c2c0e01342
Tommy Hu <tomxhu@gmail.com>
2014-11-13 14:02:43 -0500

    more refactoring

--- index.js
+++ index.js
@@ -2,81 +2,30 @@ var express = require('express')
 var app = express();
 var schedule = require('node-schedule');
 
-var twilioService = require('./twilioService');
+var utils = require('./utils/utils');
+var twilioService = require('./services/twilioService');
 var sinkData = require('./routes/sinkData');
 
 var index = process.argv[2] || 0;
 
-var numbers = [
-	'4105751082', // Dan
-	'8572075659', // Vy
-	'8573139589',  // Anu
-	'9492926781', // Mike
-	'7819568182' // Tommy
-]
-var people = ['Dan', 'Vy', 'Anu', 'Mike', 'Tommy'];
-
-// sets up rule
-var rule = {
-	hour: 9,
-	minute: 0,
-	dayOfWeek: 0
-}
-
-var testRule = new schedule.RecurrenceRule();
-rule.second = 0;
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-//
-//function sendText(number, body) {
-//	var now = new Date();
-//	var hour =  now.getHours();
-//	if (hour > 8) {
-//		client.messages.create({
-//		to: number,
-//		from: "+15082326612",
-//		body: body,
-//		}, function(err, message) {
-//			if (err) {
-//				console.log(err);
-//			}
-//			console.log('This is the messageID', message);
-//		});
-//	}
-//};
-
-function createMessage(dishes, trash) {
-	return "It is " + dishes + "'s week on dishes and " +
-			trash + "'s week on trash." 
-}
-
 var body;
-
-
 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  body = createMessage(people[index], people[(index + 2) % 5]);
+  body = createMessage(utils.people[index], utils.people[(index + 2) % 5]);
   response.send(body);
 });
 
 app.get('/sendText', function(request, response) {
-	body = createMessage(people[index], people[(index + 2) % 5]);
+	body = utils.createMessage(utils.people[index], utils.people[(index + 2) % 5]);
 	response.send('sending texts');
-	twilioService.sendText(numbers[4], body);
-	//numbers.forEach(function (number){
+	twilioService.sendText(utils.numbers[4], body);
+	//utils.numbers.forEach(function (number){
 	//	twilioService.sendText(number, body);
-	//		sleep(1000*60*2);
+	//		utils.sleep(1000*60*2);
 	//	});
 });
 
@@ -84,17 +33,17 @@ app.post('/sinkData', sinkData.sinkDataPost);
 
 app.listen(app.get('port'), function() {
 
-	var text = schedule.scheduleJob(rule, function(){
+	var text = schedule.scheduleJob(utils.rule, function(){
 		index += 1;
 		index = index % 5;
-		body = createMessage(people[index], people[(index + 2) % 5]);
-		
-		numbers.forEach(function (number){
+		body = utils.createMessage(utils.people[index], utils.people[(index + 2) % 5]);
+
+		utils.numbers.forEach(function (number){
 			twilioService.sendText(number, body);
-			sleep(1000*60*2);
+			utils.sleep(1000*60*2);
 		});
 
-		// twilioService.sendText(numbers[1], body);
+		// twilioService.sendText(utils.numbers[1], body);
 		
 		
 	});
---
+++ utils/utils.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+    numbers: [
+        '4105751082', // Dan
+        '8572075659', // Vy
+        '8573139589',  // Anu
+        '9492926781', // Mike
+        '7819568182' // Tommy
+    ],
+
+    people: ['Dan', 'Vy', 'Anu', 'Mike', 'Tommy'],
+
+    rule: {
+        hour: 9,
+        minute: 0,
+        dayOfWeek: 0
+    },
+    sleep: function(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+                break;
+            }
+        }
+    },
+
+    createMessages: function (dishes, trash) {
+        return "It is " + dishes + "'s week on dishes and " +
+            trash + "'s week on trash."
+    }
+
+}
